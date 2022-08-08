7b6e803cdb621d5b6a11ab0dcce0528302456884
WKJay <1931048074@qq.com>
2022-08-08 21:56:11 +0800

    css 初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import { defineComponent, ref } from "vue";
 import { RouterView } from "vue-router";
+import "./App.scss"
 
 export const App = defineComponent({
     setup() {
         const refCount = ref(0)
-        return () => ( <>
-        <header>
-            <ul>
-                <li><router-link to="/">Foo</router-link></li>
-                <li><router-link to="/bar">Bar</router-link></li>
-            </ul>
-        </header>
-        <div>
-            <RouterView></RouterView>
-        </div>
-        <footer>FOOTER</footer>
-        </>)
+        return () => (
+            <div>
+                <RouterView></RouterView>
+            </div>
+        )
     }
 })
